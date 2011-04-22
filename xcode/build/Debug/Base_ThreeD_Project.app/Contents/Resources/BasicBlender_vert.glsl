57ecbbd01be53e88f8b75a029725a01c0f080837
ascavarelli <user@server.com>
2011-04-22 12:19:57 -0400

    getting rid of build folder

--- xcode/build/Debug/Base_ThreeD_Project.app/Contents/Resources/BasicBlender_vert.glsl
+++
@@ -1,8 +0,0 @@
-#version 120
-
-void main()
-{
-	gl_TexCoord[0] = gl_MultiTexCoord0;
-	gl_TexCoord[1] = gl_MultiTexCoord1;
-	gl_Position = ftransform();
-}
--- xcode/build/Debug/Base_ThreeD_Project.app/Contents/Resources/NormalDepthTexCreate_frag.glsl
+++
@@ -1,9 +0,0 @@
-#version 120
-
-varying vec3 Normal;
-varying float depth;
-
-void main( void )
-{
-   gl_FragColor = vec4(normalize(Normal),depth);
-}
